739977f045b6889765f7f60a085524b279819f9d
knyme <kendidly@gmail.com>
2024-12-03 00:48:25 +0000

    a

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['i.ibb.co', 'images.unsplash.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com'],
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src')
-    return config
-  },
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './src/app/global.css'
+import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
